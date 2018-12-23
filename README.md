e3425a6a4441cd6d3eaa4f0e1c5e44319f3445ba
bonennult <bonennult@gmail.com>
2018-12-23 19:14:10 +0800

    small changes, not reliable

--- README.md
+++ README.md
@@ -2,24 +2,28 @@
 
 Use the following command to train the deep MIML network:
 ```
-python train.py --HDF5FileRoot path_of_train.h5&val.h5 --name deepMIML --checkpoints_dir checkpoints \
-  --model MIML --batchSize 256 \
+python train.py --HDF5FileRoot path_of_train.h5 \
+  --name deepMIML --checkpoints_dir checkpoints \
+  --model MIML --batchSize 64 \
+  --dataset musicInstruments \
+  --L 8 --K 1 --num_of_bases 16 \
+  --num_of_fc 1 \
   --learning_rate 0.001 \
   --learning_rate_decrease_itr 5 \
   --decay_factor 0.94 \
-  --display_freq 10 \
-  --save_epoch_freq 5 \
+  --display_freq 20 \
+  --save_epoch_freq 20 \
   --save_latest_freq 500 \
   --gpu_ids -1 --nThreads 0 \
-  --num_of_fc 1 \
   --with_batchnorm \
+  --with_softmax \
   --continue_train \
-  --niter 300 --L 15 \
+  --niter 300 \
   --validation_on \
   --validation_freq 50 \
   --validation_batches 10 \
+  --measure_time \
   --selected_classes \
-  --dataset musicInstruments \
   --using_multi_labels |& tee -a train.log
 ```
 
@@ -30,15 +34,17 @@ tensorboard --logdir=runs
 
 Use the following command to test the deep MIML network:
 ```
-python test.py --HDF5FileRoot path_of_test.h5 --name deepMIML --checkpoints_dir checkpoints \
-  --model MIML --batchSize 256 \
+python test.py --HDF5FileRoot path_of_test.h5 \
+  --name deepMIML --checkpoints_dir checkpoints \
+  --model MIML --batchSize 64 \
+  --dataset musicInstruments \
   --gpu_ids -1 --nThreads 0 \
   --num_of_fc 1 \
   --with_batchnorm \
-  --L 15 \
+  --L 8 --K 1 --num_of_bases 16 \
   --selected_classes \
   --dataset musicInstruments \
-  --using_multi_labels |& tee -a train.log
+  --using_multi_labels |& tee -a test.log
   ```
 
 Learning to Separate Object Sounds by Watching Unlabeled Video: [[Project Page]](http://vision.cs.utexas.edu/projects/separating_object_sounds/)    [[arXiv]](https://arxiv.org/abs/1712.04109)<br/>
--- dataset/NMF/audio_import.py
+++ dataset/NMF/audio_import.py
@@ -7,36 +7,56 @@ import numpy as np
 from dataset.NMF import stft_utils
 from dataset.NMF import constants
 from dataset.NMF import transformer_nmf
-
+import scipy
+from sklearn.decomposition import NMF
 '''
 filepath = "D:/shiting/audio"
 filename= os.listdir(filepath)
 stft_result = []
 nmf_result =[]
 '''
+def hello_world():
+    print("hello world")
 
 def nmf(filename):
     stft_result = []
     nmf_result =[]
     # Load audio file & compute stft
     for file in filename:
-        f = wave.open(file,'rb')
-        params = f.getparams()
-        nframes = params[3]
-        strData = f.readframes(nframes)
-        waveData = np.fromstring(strData,dtype=np.int16)
-        waveData = waveData[:44000*30]
-        win_type = constants.WINDOW_HANN
-        win_length = 4400
-        hop_length = int(win_length / 2)
-        stft = stft_utils.e_stft(signal=waveData, window_length=win_length,
-                                             hop_length=hop_length, window_type=win_type,n_fft_bins=4801)
+        stft=Stft(file)
         stft_result.append(abs(stft))
-
     # Calculate nmf for every audio
     for stft in stft_result:
-        nmf_result.append( transformer_nmf.TransformerNMF(input_matrix=stft, num_components=16,
-                                                     seed=0, should_do_epsilon=False,
-                                                     max_num_iterations=50,
-                                                     distance_measure='kl_divergence').template_dictionary)
+        #NMF =transformer_nmf.TransformerNMF(input_matrix=stft, num_components=25,
+        #                                             seed=2018,should_update_activation =True,
+         #                                            should_update_template=True,max_num_iterations=50,
+          #                                           should_do_epsilon=False,
+            #                                         distance_measure='kl_divergence')
+        #NMF.transform()
+        model = NMF(n_components=16,init='random',random_state=0)
+        W = model.fit_transform(stft)
+        H = model.components_
+        nmf_result.append(W)
     return nmf_result
+
+def Stft(Filename):
+    f = wave.open(Filename,'rb')
+    params = f.getparams()
+    #print(params)
+    nframes = params[3]
+    strData = f.readframes(nframes)
+    waveData = np.fromstring(strData,dtype=np.int16)
+    #print(waveData.shape)
+    #print("RawData:",waveData)
+    #waveData = waveData[:44000*30]
+    win_type = constants.WINDOW_HANN
+    win_length = 4800
+    hop_length = int(win_length / 2)
+    #stft = stft_utils.e_stft(signal=waveData, window_length=win_length,
+    #                                         hop_length=hop_length, window_type=win_type,n_fft_bins=4801)
+    fs = 48000
+    #window = scipy.signal.get_window('hamming',)
+    stft2 = scipy.signal.stft(x=waveData,fs=fs,nperseg=win_length,noverlap=hop_length)[2]
+    #print (stft)
+    #print (stft-stft2)
+    return stft2
